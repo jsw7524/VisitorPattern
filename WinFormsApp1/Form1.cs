662f2afd709c41c059fbe15ed8236025f5730bff
jsw7524 <jsw7524@gmail.com>
2023-11-03 17:05:10 +0800

    beta

--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -11,14 +11,14 @@ namespace WinFormsApp1
 
         Spliter spliter;
 
-        public void DrawString(int x, int y, string text)
+        public void DrawString(int x, int y, int width, int height, string text)
         {
             System.Drawing.Graphics formGraphics = this.CreateGraphics();
             string drawString = text;
-            System.Drawing.Font drawFont = new System.Drawing.Font("Arial", 6);
+            System.Drawing.Font drawFont = new System.Drawing.Font("Arial", 8);
             System.Drawing.SolidBrush drawBrush = new System.Drawing.SolidBrush(System.Drawing.Color.Black);
             System.Drawing.StringFormat drawFormat = new System.Drawing.StringFormat();
-            formGraphics.DrawString(drawString, drawFont, drawBrush, x, y, drawFormat);
+            formGraphics.DrawString(drawString, drawFont, drawBrush, new RectangleF(x,y, width, height));
             drawFont.Dispose();
             drawBrush.Dispose();
             formGraphics.Dispose();
@@ -27,7 +27,7 @@ namespace WinFormsApp1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Directory root = new Directory("D:\\Demo\\ConsoleApp1\\");
+            Directory root = new Directory(@"D:\國庫局機器學習研究案\Predict\");
 
             spliter = new Spliter();
             spliter.Split(root.Entries, Spliter.Direction.HORIZONTAL, 0, 0, 1800, 900);
@@ -47,8 +47,16 @@ namespace WinFormsApp1
                     {
                         foreach (Block blk in spliter.blocks)
                         {
+                            //////////////////
+                            //var label = new Label();
+                            //label.Location = new Point(blk.x1+30, blk.y1+30);
+                            //label.Visible = true;
+                            //label.Size =new Size(blk.x2 - blk.x1 -30, blk.y2 - blk.y1-30);
+                            //label.Text = blk.Name;
+                            //Controls.Add(label);
                             g.DrawRectangle(selPen, blk.x1, blk.y1, blk.x2 - blk.x1, blk.y2 - blk.y1);
-                            DrawString(blk.x1, blk.y1, blk.Name + " " + blk.Size);
+                            DrawString(blk.x1, blk.y1, blk.x2 - blk.x1, blk.y2 - blk.y1, blk.Name + " " + Math.Round(blk.Size/1023.0)+"KB");
+                            //////////////////
                         }
                     }
 
